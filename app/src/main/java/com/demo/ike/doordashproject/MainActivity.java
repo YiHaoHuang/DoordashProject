2ec30dfd4bfcbb7543cb5e3bfad085bf35789583
ike <ike@oath.com>
2018-11-03 02:58:57 -0700

    Initial commit

---
+++ app/src/main/java/com/demo/ike/doordashproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.demo.ike.doordashproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
